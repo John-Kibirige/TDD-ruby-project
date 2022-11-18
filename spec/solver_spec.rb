3f59287bd0121df8be63706e582f581e0b92e5f5
John <kibirigejohn005@gmail.com>
2022-11-18 07:19:47 +0300

    write a failing test that checks if the factorial parameter passed is less than 0

---
+++ spec/solver_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+
+RSpec.describe 'Solver' do 
+    it 'check if the right parameters are passed to factorial method' do 
+        solver = Solver.new 
+        expect{solver.factorial(-1)}.to raise_exception(StandardError) 
+    end
+end
